9c4b4e3f92675a03db4892782f1969e77f0951d1
Taha Enes Aslantürk <enes.aslanturk@ug.bilkent.edu.tr>
2022-04-25 19:08:32 +0300

    Initial Commit

---
+++ Magic Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic Ball
+//
+//  Created by Taha Enes Aslantürk on 25.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
